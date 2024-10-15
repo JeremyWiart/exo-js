8f661e93c31714e19a367b77e1471df4e1ebec71
Jeremy <jeremy.wiart@outlook.com>
2024-10-15 21:02:46 +0200

    Add files via upload

---
+++ occu.js
@@ -0,0 +1,15 @@
+let liste = [8, 7, 3, 8, 3, 1, 8, 8];
+let elements = 8;
+function occurrences(liste, elements) {
+    let ranges = [];
+
+  
+    liste.forEach((element, index) => {
+        if (element === elements) {
+            ranges.push([index, index]); 
+        }
+    });
+
+    return ranges;
+}
+console.log(occurrences(liste, elements)); 
